95c3b79a8629200fbf5697326ba6aa769ef6fc30
waleG <168023111+waleGGGGG@users.noreply.github.com>
2024-08-03 12:21:00 +0300

    Delete lesson_18 directory

--- lesson_18/js/JavaScript.js
+++
@@ -1,9 +0,0 @@
-// керування бургером
-
-const toggleButton = document.getElementById('toggleButton');
-function toggleTransform() {
-    toggleButton.classList.toggle('active');
-}
-toggleButton.addEventListener('blur', () => {
-    toggleButton.classList.remove('active');
-});
